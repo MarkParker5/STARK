c0fcbe08dd44e92c3c771ce608f9d475c584136d
MarkParker5 <34688010+MarkParker5@users.noreply.github.com>
2020-08-30 00:47:53 +0300

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-telebot            = '1324863832:AAEcBXMcCXxrH-PnscUo6c2VkrCkqxJHiPs'
-goole_tts_json_key = 'tts-gc-key.json'
-
-language_code      = 'ru-RU'
-device_index       = 1
-
-names              = ['арчи', 'archie']
