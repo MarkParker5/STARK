8c5d6215f005ce264ddcc362bea77eefae33df70
MarkParker5 <markitstep@gmail.com>
2020-10-31 12:04:13 +0200

    Pi smart cooling

--- pi_temp.py
+++ pi_temp.py
@@ -3,14 +3,16 @@ import time
 import RPi.GPIO as GPIO
 
 pin = 3
+cooling = False
 
 def current_temp():
     temp = os.popen("vcgencmd measure_temp").readline()
     return temp.replace("temp=","")
 
+GPIO.setmode(GPIO.BCM)
 GPIO.setup(pin, GPIO.OUT)
 while True:
-    temp = int(current_temp())
+    temp = float(current_temp()[:4])
     if temp > 55:
         cooling = True
     elif temp < 40:
