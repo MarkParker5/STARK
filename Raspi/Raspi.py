627ff20c826b456e4faf922f9a6f9375e1ccd438
MarkParker5 <markitstep@gmail.com>
2020-11-28 08:22:14 +0200

    small fix

--- Raspi/Raspi.py
+++ Raspi/Raspi.py
@@ -4,5 +4,6 @@ class Raspi(Command):
     def start(this, string):                    #   main method
         pass
 
-    def hdmi_cec(this, command):
+    @staticmethod
+    def hdmi_cec(command):
         os.system(f'echo \'{command}\' | cec-client -s -d 1')
