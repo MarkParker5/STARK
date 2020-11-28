88c735a688c02666a06697ae4dc62d8f00139bfa
MarkParker5 <markitstep@gmail.com>
2020-11-28 08:36:59 +0200

    small fix

--- Raspi/tv.py
+++ Raspi/tv.py
@@ -30,20 +30,6 @@ tv_off = Raspi('tv off', keywords, patterns)
 tv_off.setStart(method)
 ################################################################################
 def method(params):
-    Raspi.hdmi_cec('tx 4F:82:20:00')
-    voice = text = ''
-    return {
-        'type': 'simple',
-        'text': text,
-        'voice': voice,
-    }
-
-keywords = {}
-patterns = ['* (выведи|вывести|покажи|открой|показать|открыть) * с (ноута|ноутбука|провода|hdmi) *']
-tv_hdmi = Raspi('tv hdmi source', keywords, patterns)
-tv_hdmi.setStart(method)
-################################################################################
-def method(params):
     port = params['num'] + '0' if len(params['num']) == 1 else params['num']
     Raspi.hdmi_cec(f'tx 4F:82:{port}:00')
     voice = text = ''
@@ -55,6 +41,20 @@ def method(params):
 
 keywords = {}
 patterns = ['* (выведи|вывести|покажи|открой|показать|открыть) * с (|провода|hdmi|кабеля|порта) * $num *']
+tv_hdmi = Raspi('tv hdmi source', keywords, patterns)
+tv_hdmi.setStart(method)
+################################################################################
+def method(params):
+    Raspi.hdmi_cec('tx 4F:82:20:00')
+    voice = text = ''
+    return {
+        'type': 'simple',
+        'text': text,
+        'voice': voice,
+    }
+
+keywords = {}
+patterns = ['* (выведи|вывести|покажи|открой|показать|открыть) * с (ноута|ноутбука|провода|hdmi)']
 tv_hdmi = Raspi('tv hdmi source', keywords, patterns)
 tv_hdmi.setStart(method)
 ################################################################################
