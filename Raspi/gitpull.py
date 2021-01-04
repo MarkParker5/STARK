b4e08f94111700960f84d95a6c1ecb0e1e5e670a
MarkParker5 <markitstep@gmail.com>
2021-01-04 20:08:16 +0200

    change reboot method

--- Raspi/gitpull.py
+++ Raspi/gitpull.py
@@ -5,7 +5,7 @@ import config
 ################################################################################
 def reboot(params):
     if params['bool']:
-        os.system('sudo systemctl restart archie')
+        os.system('sudo reboot')
     return {
         'text': 'Хорошо',
         'voice': 'Хорошо',
