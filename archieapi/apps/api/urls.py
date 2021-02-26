ecc93e5ac93df01ed4b2537a67a939cef6c943ab
MarkParker5 <markitstep@gmail.com>
2021-02-27 01:40:52 +0200

    fix

--- archieapi/apps/api/urls.py
+++ archieapi/apps/api/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
 	path('text', views.text, name = 'text'),
-	path('command', views.commands, name = 'commands'),
+	path('command', views.command, name = 'command'),
 ]
