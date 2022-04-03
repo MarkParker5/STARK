44e4aa48f92b39231220471d25e704cb1626ef39
MarkParker5 <markparker.it@gmail.com>
2022-04-03 17:53:41 +0200

    Update merlin

--- General/Merlin/Merlin.py
+++ General/Merlin/Merlin.py
@@ -1,17 +1,18 @@
+from typing import List
+from threading import Thread
 import RPi.GPIO as GPIO
-from .lib_nrf24 import NRF24
 import spidev
 
-from threading import Thread
-from ArchieCore import Command
+# from ArchieCore import Command
 from .MerlinMessage import MerlinMessage
+from .lib_nrf24 import NRF24
 
 GPIO.setmode(GPIO.BCM)
 
 class Merlin():
-    my_urdi = [0x00, 0x00, 0x00, 0x01]
     radio: NRF24
     send_queue: List[MerlinMessage] = []
+    my_urdi = [0xf0, 0xf0, 0xf0, 0xf0, 0xe1]
 
     def __init__(self):
         radio = NRF24(GPIO, spidev.SpiDev())
@@ -21,7 +22,7 @@ class Merlin():
         radio.setChannel(0x60)
         radio.setPayloadSize(2)
         radio.setAutoAck(True)
-        radio.setAddressWidth(4)
+        #radio.setAddressWidth(4)
         radio.enableAckPayload()
         radio.openReadingPipe(1, self.my_urdi)
 
@@ -53,7 +54,7 @@ class Merlin():
 
             rawData = []
             self.radio.read(rawData, self.radio.getPayloadSize())
-            func, arg = *rawData
+            func, arg = rawData
             print(f'{func=} {arg=}')
 
 receiveAndTransmitThread = Thread(target=Merlin().receiveAndTransmit)
