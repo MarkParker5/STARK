7cfc87c8d59a1e85c68e4c6cc4c707ce580cd711
MarkParker5 <markparker.it@gmail.com>
2022-04-27 00:17:13 +0200

    add API to start.py, update dependencies

--- start.py
+++ start.py
@@ -3,38 +3,45 @@
 
 import os, sys
 import multiprocessing
+import uvicorn
 
 import Features
 import Controls
-
 from Controls.API.main import app
+
+
+def start_api():
+    uvicorn.run('start:app')
 
 def multiprocess():
     controls = [
         Controls.VoiceAssistant(),
         Controls.TelegramBot(),
-        #RemoteControl(),
-        #Django(),
     ]
 
+    starts = [control.start for control in controls]
+    starts.append(start_api)
+
     processes = []
-    for control in controls:
-        process = multiprocessing.Process(target = control.start)
+    for start in starts:
+        process = multiprocessing.Process(target = start)
         process.start()
+        processes.append(process)
 
     for process in processes:
         process.join()
 
 def multiterminal():
     controls = {
-        'VoiceAssistant': Controls.VoiceAssistant,
-        'TelegramBot': Controls.TelegramBot
+        'VoiceAssistant': Controls.VoiceAssistant().start,
+        'TelegramBot': Controls.TelegramBot().start,
+        'API': start_api,
     }
 
     if len(sys.argv) > 1:
-        controls.get(sys.argv[1])().start()
+        controls.get(sys.argv[1])()
     else:
         for key in controls.keys():
             os.system(f'lxterminal --command="python start.py {key}"')
 
-if __name__ == '__main__': pass#multiterminal()
+if __name__ == '__main__': multiprocess()
