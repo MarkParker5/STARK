7b8d17743716cb07e25984b8bb48633ac94156b3
MarkParker5 <markparker.it@gmail.com>
2022-06-20 17:59:45 +0200

    add hub tests

--- SmartHome/tests/test_endpoints/test_hub.py
+++ SmartHome/tests/test_endpoints/test_hub.py
@@ -1,22 +1,35 @@
 from tests.setup import *
 
 
-def test_init_hub():
-    id = uuid1()
-    house_id = uuid1()
+id = uuid1()
+house_id = uuid1()
+default_name = 'Inited Hub'
 
+def test_get_hub_404():
+    response = client.get('/api/hub')
+    assert response.status_code == 404
+    assert response.json() == {'detail': 'Not found'}
+
+
+def test_init_hub():
     response = client.post('/api/hub', json = {
         'id': str(id),
-        'name': 'Inited Hub',
+        'name': default_name,
         'house_id': str(house_id),
         'access_token': hub_access_token,
         'refresh_token': hub_refresh_token,
         'public_key': public_key,
     })
-
     assert response.status_code == 200
 
-# Patch
+def test_get_hub():
+    response = client.get('/api/hub')
+    assert response.status_code == 200
+    assert response.json() == {
+        'id': str(id),
+        'name': default_name,
+        'house_id': str(house_id),
+    }
 
 def test_patch_hub():
     hub = client.get('/api/hub').json()
