ad3b4002135e4396318a923baedae8e72965363a
MarkParker5 <markparker.it@gmail.com>
2022-06-23 01:21:39 +0200

    small fix

--- SmartHome/API/dependencies/database.py
+++ SmartHome/API/dependencies/database.py
@@ -40,6 +40,5 @@ create_async_session = sessionmaker(
 )
 
 async def get_async_session() -> AsyncSession:
-    raise Exception('FUUCK')
     async with create_async_session() as session:
         yield session
