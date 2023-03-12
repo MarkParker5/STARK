b8d62a933accc92e461ee44a14a43a8bad27a4c5
MarkParker5 <markparker.it@gmail.com>
2023-03-12 15:43:21 +0100

    minor improvements

--- majordom_va/config.py
+++ majordom_va/config.py
@@ -3,8 +3,8 @@
 vosk_model: str = 'downloads/vosk_small_rus'
 
 # Google Cloud TTS
+goole_tts_json_key: str = 'downloads/gc-tts-key.json'
 language_code: str = 'ru-RU'
-goole_tts_json_key: str = 'tts-gc-key.json'
 
 # Silero TTS
 silero_model_url: str = 'https://models.silero.ai/models/tts/ru/ru_v3.pt'
