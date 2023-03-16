f4a5263a04a9944f1ced9d84b112730ec0afa468
MarkParker5 <markparker.it@gmail.com>
2023-03-16 11:52:42 +0100

    test_repeating_command

--- tests/test_commands_flow/test_multiple_commands.py
+++ tests/test_commands_flow/test_multiple_commands.py
@@ -17,17 +17,26 @@ def test_multiple_commands(commands_context_flow):
     assert context_delegate.responses[0].text == 'foo!'
     assert context_delegate.responses[1].text == 'lorem!'
 
-def test_repeating_command():
+def test_repeating_command(commands_context_flow):
+    manager, context, context_delegate = commands_context_flow
+    
+    @manager.new('lorem * dolor')
+    def lorem(): 
+        return Response(text = 'lorem!')
+    
+    context.process_string('lorem pisum dolor lorem ipsuuum dolor sit amet')
+    assert len(context_delegate.responses) == 2
+    assert context_delegate.responses[0].text == 'lorem!'
+    assert context_delegate.responses[1].text == 'lorem!'
+
+def test_overlapping_commands_less_priority_cut(commands_context_flow):
     pass
 
-def test_overlapping_commands_less_priority_cut():
+def test_overlapping_commands_priority_cut(commands_context_flow):
     pass
 
-def test_overlapping_commands_priority_cut():
+def test_overlapping_commands_remove(commands_context_flow):
     pass
 
-def test_overlapping_commands_remove():
-    pass
-
-def test_viobjects_parse_caching():
+def test_viobjects_parse_caching(commands_context_flow):
     pass
