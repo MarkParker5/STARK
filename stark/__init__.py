f3163a6193c9f948356959d9a44671f5c6da3cd2
Mark Parker <mark@parker-programs.com>
2023-09-17 14:01:28 +0200

    run function

---
+++ stark/__init__.py
@@ -0,0 +1,31 @@
+import asyncer
+
+from stark.interfaces.protocols import SpeechRecognizer, SpeechSynthesizer
+from stark.core import CommandsContext, CommandsManager
+from stark.voice_assistant import VoiceAssistant
+from stark.general.blockage_detector import BlockageDetector
+
+
+async def run(
+    manager: CommandsManager,
+    speech_recognizer: SpeechRecognizer,
+    speech_synthesizer: SpeechSynthesizer
+):
+    async with asyncer.create_task_group() as main_task_group:
+        context = CommandsContext(
+            task_group = main_task_group, 
+            commands_manager = manager
+        )
+        voice_assistant = VoiceAssistant(
+            speech_recognizer = speech_recognizer,
+            speech_synthesizer = speech_synthesizer,
+            commands_context = context
+        )
+        speech_recognizer.delegate = voice_assistant
+        context.delegate = voice_assistant
+        
+        main_task_group.soonify(speech_recognizer.start_listening)()
+        main_task_group.soonify(context.handle_responses)()
+        
+        detector = BlockageDetector(threshold = 1)
+        main_task_group.soonify(detector.monitor)()
