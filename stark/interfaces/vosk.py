ab5b7ae931aaa146eb618c558e1f196afd541328
Mark Parker <mark@parker-programs.com>
2023-09-21 18:53:14 +0200

    wip: vosk speaker ID

--- stark/interfaces/vosk.py
+++ stark/interfaces/vosk.py
@@ -1,4 +1,5 @@
-from typing import Optional
+from typing import Optional, cast
+from datetime import datetime, timedelta
 import os
 import urllib.request
 import zipfile
@@ -19,7 +20,6 @@ class VoskSpeechRecognizer(SpeechRecognizer):
     _delegate: SpeechRecognizerDelegate | None = None
 
     audio_queue: Queue
-    model: vosk.Model
 
     samplerate: int
     blocksize = 8000
@@ -27,16 +27,21 @@ class VoskSpeechRecognizer(SpeechRecognizer):
     channels = 1
     kaldiRecognizer: vosk.KaldiRecognizer
 
-    last_result: Optional[str] = ""
-    last_partial_result: str = ""
+    last_result: Optional[str] = ''
+    last_partial_result: str = ''
+    last_partial_update_time: Optional[datetime] = None
 
     is_recognizing = True
     _is_listening = False
+    
+    _stored_speakers: dict[int, list[int]] = {}
+    _speaker_trashold = 0.75
 
-    def __init__(self, model_url: str):
+    def __init__(self, model_url: str, speaker_model_url: str | None = None):
         downloads = 'downloads'
         model_path = downloads + '/' + model_url.split('/')[-1].replace('.zip', '')
         zip_path = model_path + '.zip'
+        speaker_model_path = downloads + '/' + speaker_model_url.split('/')[-1].replace('.zip', '') if speaker_model_url else None
         
         if not os.path.isdir('downloads'):
             os.mkdir('downloads')
@@ -48,11 +53,22 @@ class VoskSpeechRecognizer(SpeechRecognizer):
             zip_file.extractall(downloads)
             os.remove(zip_path)
             print('VOSK: Model downloaded!')
+            
+        if speaker_model_url and not os.path.isdir(cast(str, speaker_model_path)):
+            print('VOSK: Downloading speaker model...')
+            urllib.request.urlretrieve(speaker_model_url, zip_path)
+            zip_file = zipfile.ZipFile(zip_path)
+            zip_file.extractall(downloads)
+            os.remove(zip_path)
+            print('VOSK: Speaker model downloaded!')
         
-        self.samplerate = int(sounddevice.query_devices(kind = 'input')['default_samplerate'])
-        self.model = vosk.Model(model_path)
         self.audio_queue = Queue()
-        self.kaldiRecognizer = vosk.KaldiRecognizer(self.model, self.samplerate)
+        self.samplerate = int(sounddevice.query_devices(kind = 'input')['default_samplerate'])
+        vosk_model = vosk.Model(model_path)
+        speaker_model = vosk.SpkModel(speaker_model_path) if speaker_model_path else None
+        self.kaldiRecognizer = vosk.KaldiRecognizer(vosk_model, self.samplerate)
+        if speaker_model_url:
+            self.kaldiRecognizer.SetSpkModel(speaker_model)
         
     @property
     def delegate(self):
@@ -79,7 +95,9 @@ class VoskSpeechRecognizer(SpeechRecognizer):
 
     async def start_listening(self):
         if self._is_listening: return
-        
+
+        self.last_partial_result = ''
+        self.last_partial_update_time = None
         self._is_listening = True
 
         with sounddevice.RawInputStream(**self.sounddevice_parameters):
@@ -89,26 +107,76 @@ class VoskSpeechRecognizer(SpeechRecognizer):
                         await self._transcribe(data)
                 except Empty:
                     pass
-                await anyio.sleep(0.05)
+                await anyio.sleep(0.01)
                 
     async def _transcribe(self, data):
         delegate = self.delegate
         if not delegate: return
         
         if self.kaldiRecognizer.AcceptWaveform(data):
+            self.last_partial_update_time = None
             result = json.loads(self.kaldiRecognizer.Result())
-            if (string := result.get('text')) and string != self.last_result:
+            
+            if (string := result.get('text')):
                 self.last_result = string
                 await delegate.speech_recognizer_did_receive_final_result(string)
             else:
                 self.last_result = None
                 await delegate.speech_recognizer_did_receive_empty_result()
+            
+            # if spk := result.get('spk'):
+            #     speaker, similarity = self._get_speaker(spk)
+            #     print(f'\nSpeaker: {speaker} ({similarity * 100:.2f}%)\n')
+                
         else:
             result = json.loads(self.kaldiRecognizer.PartialResult())
+            
             if (string := result.get('partial')) and string != self.last_partial_result:
                 self.last_partial_result = string
+                self.last_partial_update_time = datetime.now()
                 await delegate.speech_recognizer_did_receive_partial_result(string)
+                
+        # Check for partial results timeout 
+        # TODO: (was bug with stucked partial results, need to check again)
+        # TODO: check last string didn't change all timeout
+        # if not self.last_partial_update_time: 
+        #     return
+
+        # if datetime.now() - self.last_partial_update_time > timedelta(seconds = 1):
+        #     print('\nPartial timeout')
+        #     self.last_partial_update_time = None
+            
+        #     if self.last_partial_result:
+        #         await delegate.speech_recognizer_did_receive_final_result(self.last_partial_result)
+        #         self.kaldiRecognizer.Reset() # avoid duplicate results
+        #         self.last_partial_result = ''
 
     def _audio_input_callback(self, indata, frames, time, status):
         if not self.is_recognizing: return
         self.audio_queue.put(bytes(indata))
+        
+    def _get_speaker(self, vector: list[int]) -> tuple[int, float]:
+        # TODO: search Is not working good, need to improve the algorithm
+        
+        matched_speaker_id = None
+        best_similarity = -1.0
+        
+        for speaker_id, vec in self._stored_speakers.items():
+            similarity = self._cosine_similarity(vector, vec)
+            if similarity > best_similarity:
+                best_similarity = similarity
+                matched_speaker_id = speaker_id
+                
+        if not best_similarity or best_similarity < self._speaker_trashold:
+            matched_speaker_id = len(self._stored_speakers)
+            self._stored_speakers[matched_speaker_id] = vector
+            print(f'New speaker: {matched_speaker_id}, similarity: {best_similarity * 100:.2f}%')
+            best_similarity = 1
+            
+        return cast(int, matched_speaker_id), best_similarity
+        
+    def _cosine_similarity(self, vector_a, vector_b) -> float:
+        dot_product = sum(a*b for a, b in zip(vector_a, vector_b))
+        norm_a = sum(a*a for a in vector_a) ** 0.5
+        norm_b = sum(b*b for b in vector_b) ** 0.5
+        return dot_product / (norm_a * norm_b)
