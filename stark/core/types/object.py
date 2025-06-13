40ec6df7c9ad3967458d5ec7a076a389e3265ca9
Mark Parker <mark@parker-programs.com>
2025-06-13 16:32:10 +0200

    more checks and constraints for did_parse

--- stark/core/types/object.py
+++ stark/core/types/object.py
@@ -16,7 +16,7 @@ class ParseError(Exception):
 class Object(ABC):
 
     value: Any
-    
+
     @classproperty
     def pattern(cls) -> Pattern:
         return Pattern('**')
@@ -24,17 +24,17 @@ class Object(ABC):
     def __init__(self, value: Any):
         '''Just init with wrapped value.'''
         self.value = value
-        
+
     async def did_parse(self, from_string: str) -> str:
         '''
-        This method is called after parsing from string and setting parameters found in pattern. 
+        This method is called after parsing from string and setting parameters found in pattern.
         You will very rarely, if ever, need to call this method directly.
-        
-        Override this method for more complex parsing from string. 
-        
+
+        Override this method for more complex parsing from string.
+
         Returns:
             Minimal substring that is required to parse value.
-        
+
         Raises:
             ParseError: if parsing failed.
         '''
@@ -47,27 +47,30 @@ class Object(ABC):
         For internal use only.
         You will very rarely, if ever, need to override or even call this method.
         Override `def did_parse(self, from_string: str) -> str` if you need complex parsing.
-        
+
         Raises:
             ParseError: if parsing failed.
         '''
-        
+
         obj = cls(None)
         parameters = parameters or {}
-        
+
         for name, object_type in cls.pattern.parameters.items():
             if not parameters.get(name):
                 continue
             value = parameters.pop(name)
+            # TODO: check whether all pattern parameters must be assigned as object attributes
             setattr(obj, name, (await object_type.parse(from_string = value, parameters = parameters)).obj)
-        
+
         substring = await obj.did_parse(from_string)
-        
+        assert substring in from_string, ValueError(f'Parsed substring must be a part of the original string. There is no {substring} in {from_string}.')
+        assert obj.value is not None, ValueError(f'Parsed object {obj} must have a `value` property set in did_parse method. It is not set in {type(obj)}.')
+
         return ParseResult(obj, substring)
-    
+
     def copy(self) -> Object:
         return copy.copy(self)
-    
+
     def __format__(self, spec) -> str:
         return f'{self.value:{spec}}'
 
@@ -79,4 +82,3 @@ class Object(ABC):
         if not isinstance(other, type(self)):
             raise NotImplementedError(f'Cannot compare {type(self)} with {type(other)}')
         return self.value == other.value
-    
