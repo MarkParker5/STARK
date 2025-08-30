ee2acea3b89525dcb4e796cb1f7f31af79e3eaf6
Mark Parker <mark@parker-programs.com>
2025-08-30 20:15:32 +0200

    improve classproperty type annotation

--- stark/general/classproperty.py
+++ stark/general/classproperty.py
@@ -1,4 +1,23 @@
+from typing import Callable
 
-class classproperty(property): # TODO: typing
-    def __get__(self, cls, owner):
-        return classmethod(self.fget).__get__(None, owner)()
+
+class classproperty[GetterReturnType]:
+    def __init__(self, func: Callable[..., GetterReturnType]):
+        if isinstance(func, (classmethod, staticmethod)):
+            fget = func
+        else:
+            fget = classmethod(func)
+        self.fget = fget
+
+    def __get__(self, obj, klass=None) -> GetterReturnType:
+        if klass is None:
+            klass = type(obj)
+        return self.fget.__get__(obj, klass)()
+
+# from typing import Any, Callable
+
+# def classproperty[T](fget: Callable[[type[Any]], T]) -> T:
+#     class _ClassProperty(property):
+#         def __get__(self, cls: type[Any], owner: type[Any]) -> T: # type: ignore
+#             return classmethod(fget).__get__(None, owner)()
+#     return _ClassProperty(fget) # type: ignore
