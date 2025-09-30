62917dcb2bf9577d188d0c0e1a942af482c9b440
Mark Parker <mark@parker-programs.com>
2025-09-30 21:06:32 +0200

    fix the levenshtein, tests WIP [no ci]

--- stark/tools/levenshtein.py
+++ stark/tools/levenshtein.py
@@ -35,20 +35,21 @@ SIMPLEPHONE_PROXIMITY_GRAPH: ProximityGraph = {
 # if not s2 or (skip_spaces and s2.replace(' ', '') == ''):
 #     return (len(s2), 0)
 
-def levenshtein_table(
+def levenshtein_matrix(
     s1: str,
     s2: str,
     max_distance: float | None = None,
-    square: bool = False,
     narrow: bool = True,
     proximity_graph: ProximityGraph = {},
 ) -> np.ndarray:
     '''
     Calculates the Levenshtein distance between two strings s1 and s2.
-
+    Max distance stops the calculations early if the distance exceeds the max_distance for better performance.
+    Narrow mode minimizes the number of operations, limits max distance to the smaller of the two strings; calculates "substring" match instead of full string match.
+    Proximity graph allows tweaking the cost of operations for any character (default cost is 1), for example, based on phonetic similarity, keyboard proximity, char aliases, costless whitespaces, etc.
     '''
 
-    abs_max_distance = min(len(s1), len(s2)) if square else max(len(s1), len(s2))
+    abs_max_distance = min(len(s1), len(s2)) if narrow else max(len(s1), len(s2))
     max_distance = max_distance if max_distance is not None else abs_max_distance
 
     # Start the main part
@@ -72,7 +73,8 @@ def levenshtein_table(
 
             char2 = s2[cell_i - 1]
 
-            if char1 == char2: # full match, no cost added
+            if char1 == char2 and matrix[row_i - 1, cell_i - 1] != 1e6: # check for 1e6 in narrow mode
+                # full match, no cost added
                 cell_value = matrix[row_i - 1, cell_i - 1]
 
             else: # different characters, calculate additional cost
@@ -88,14 +90,8 @@ def levenshtein_table(
                     matrix[row_i - 1, cell_i - 1] + sub_cost # substitution
                 )
 
-            matrix[row_i, cell_i] = cell_value
-
-            # early stop by max distance limit
-            if (
-                cell_value > matrix[row_i, cell_i-1] # derivative sign turned positive -> the prev was the best
-                and matrix[row_i, cell_i-1] > max_distance # the best is worse than limit
-            ):
-                return matrix
+            matrix[row_i, cell_i] = cell_value = min(cell_value, 1e6)
+            logger.debug(f"Cell ({row_i}={char1}, {cell_i}={char2}) value: {cell_value}")
 
             # cut off corners if optimisation param is set
             if narrow:
@@ -106,11 +102,17 @@ def levenshtein_table(
 
                 # remove top right corner
                 if (
-                    cell_value > matrix[row_i, cell_i-1] # derivative sign turned positive -> the prev was the best
+                    cell_value > matrix[row_i, cell_i-1] # derivative sign is positive -> the prev was the best
                     and row_i != s1_len # don't skip the last row to set the right bottom (-1;-1) cell which represents the full distance
                 ):
                     # end this row if the path is already too far
+                    logger.debug('Skipping row')
                     break
+
+        # early stop by max distance limit
+        if matrix[row_i, best_column] > max_distance: # the best is worse than limit
+            logger.debug('Distance limit')
+            return matrix
 
     # return fully filled dp
     return matrix
@@ -119,69 +121,57 @@ def levenshtein_distance(
     s1: str,
     s2: str,
     max_distance: float | None = None,
-    skip_spaces: bool = False,
-    square: bool = False,
+    narrow: bool = False,
     proximity_graph: ProximityGraph = {}
-) -> tuple[float, int]:
+) -> tuple[int, float]:
     '''
-    Optimized Levenshtein distance calculation with early return when distance exceeds threshold, with proximity graph for chars, and skipping spaces.
-
-    s2 should be the longer string.
-
-    Square: limits the maximum distance to the length of the longer string.
-
-    Returns the distance and the length of the match.
-
-    The length is only used to adjust the length of the matched s2 substring with spaces (basically len of s2 + spaces in s2)
+    Returns the best length of s2 substr for levenshtein match with s1, and the best distance for that length
     '''
-
-    dp = levenshtein_table(s1, s2, max_distance, skip_spaces, square, proximity_graph)
-    distance = np.min(dp[-1, :])
-    # distance = np.min(dp[-1, :furthest_k])
-    # distance = dp[-1, furthest_k]
-    # length = furthest_k
-    length = np.argmin(dp[-1, :])
-    logger.debug(f"Returning {distance=} {length=}")
-    return float(distance), int(length)
+    matrix = levenshtein_matrix(s1, s2, max_distance, narrow, proximity_graph)
+    best_length = np.argmin(matrix[-1, :])
+    best_distance_for_length = matrix[-1, best_length]
+    logger.debug(f"Returning {best_length=} {best_distance_for_length=}")
+    return int(best_length), float(best_distance_for_length)
 
 def levenshtein_similarity(
     s1: str,
     s2: str,
     min_similarity: float = 0,
-    skip_spaces: bool = False,
-    square: bool = False,
+    narrow: bool = False,
     proximity_graph: ProximityGraph = {}
-) -> tuple[float, int]:
+) -> tuple[int, float]:
     '''
-    Computes the similarity between two strings using the Levenshtein distance. Output: 0...1 where 1 indicates perfect similarity and 0 indicates perfect mismatch. The length of s1 is used to calculate the maximum possible distance. Returns the similarity score and the number of spaces skipped.
+    Computes the similarity between two strings using the Levenshtein distance. Output: 0...1 where 1 indicates perfect similarity and 0 indicates perfect mismatch. The length of s1 is used to calculate the maximum possible distance. Returns the best length of s2 to maximally match s1, and the similarity score.
     '''
 
     if not s1 or not s2:
         return 0, len(s1) # avoid zero division
 
     tolerance = 1 - min_similarity
-    max_total_distance = len(s1.replace(' ', '') if skip_spaces else s1)
+    # max_total_distance = len(s1.replace(' ', '') if skip_spaces else s1)
+    max_total_distance = min(len(s1), len(s2)) if narrow else max(len(s1), len(s2))
     max_allowed_distance = round(max_total_distance * tolerance)
-    distance, length = levenshtein_distance(s1, s2, max_allowed_distance, skip_spaces, square=square, proximity_graph=proximity_graph)
+    length, distance = levenshtein_distance(s1, s2, max_allowed_distance, narrow, proximity_graph=proximity_graph)
 
-    return 1 - distance / max_total_distance, length
+    return length, 1 - distance / max_total_distance
 
 def levenshtein_match(
     s1: str,
     s2: str,
     min_similarity: float,
-    skip_spaces: bool = False,
-    square: bool = False,
+    narrow: bool = False,
     proximity_graph: ProximityGraph = {}
-) -> tuple[bool, int]:
-    similarity, length = levenshtein_similarity(s1, s2, min_similarity, skip_spaces, square=square, proximity_graph=proximity_graph)
-    return similarity >= min_similarity, length
+) -> tuple[int, bool]:
+    '''
+    Checks if two strings are similar enough based on the Levenshtein distance. Returns the best length of s2 to maximally match s1, and whether it's a match. Length value is only meaningful when the match is true.
+    '''
+    length, similarity = levenshtein_similarity(s1, s2, min_similarity, narrow, proximity_graph=proximity_graph)
+    return length, similarity >= min_similarity
 
 def levenshtein_substrings_search(
     query: str,
     string: str,
     min_similarity: float,
-    skip_spaces: bool = False,
     proximity_graph: ProximityGraph = {}
 ) -> Generator[str, None, None]:
     '''
@@ -204,20 +194,25 @@ def levenshtein_substrings_search(
             # string with leading space and without are the same, so we can skip it
             continue
 
-        is_match, length = levenshtein_match(query, string[i:], min_similarity, skip_spaces=skip_spaces, square=True, proximity_graph=proximity_graph)
+        is_match, length = levenshtein_match(query, string[i:], min_similarity, narrow=True, proximity_graph=proximity_graph)
         if is_match:
             yield string[i:i + length].strip() # TODO: return span indices
-            i += length # skip matched substring; TODO: review cases of overlapping improving or degrading results
-
-# TODO: CHECK: cutting the left part of the string can give better results
-# TODO: CHECK: right part isn't cutting well too
+            i += length # skip matched substring;
+            # TODO: review cases of overlapping improving or degrading results
+            # TODO: cutting the left part of the string can give better results
+            # TODO: right part isn't cutting well too
 
 if __name__ == '__main__':
 
-    dp = levenshtein_table(
+    logging.basicConfig()
+    logger.setLevel(logging.DEBUG)
+
+    dp = levenshtein_matrix(
         # s1 := 'abcl n k n p k',
         # s2 := 'abclnknpk sit amet',
-        # s1 := 'Facebooke',
+        # s1 := 'abcl n p n k zzz',
+        # s2 := 'abclnknpk sit amet',
+        # s1 := 'Facebookeeee',
         # s2 := 'google',
         s1 := 'google',
         s2 := 'Facebookeeee',
